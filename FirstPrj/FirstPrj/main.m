12399c1bdef6f55be4ab9b1e72495974c0d4ca1b
adorno <pch4623@gmail.com>
2020-06-03 23:30:36 +0900

    dot operation

--- FirstPrj/FirstPrj/main.m
+++ FirstPrj/FirstPrj/main.m
@@ -12,7 +12,7 @@
 #import <Foundation/Foundation.h>
 
 @interface Vehicle : NSObject {
-    // member variable
+    // member variable (선언 생략 가능)
     int wheels;
     int seats;
 }
@@ -57,13 +57,20 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // instance object 생성
         Vehicle *vehicle = [[Vehicle alloc]init];   // [Vehicle new];
+        Vehicle *vehicle2 = [[Vehicle alloc]init];
         
         //[Receiver Message]
         [vehicle setWheels:4];
         [vehicle setSeats:2];
         //[vehicle print];
         
+        // dot operations
+        vehicle2.wheels = 12;
+        vehicle2.seats = 6;
+        
+        
         NSLog(@"wheels : %i, seats : %i", [vehicle wheels], [vehicle seats]);
+        NSLog(@"wheels : %i, seats : %i", vehicle2.wheels, vehicle2.seats);
     }
     return 0;
 }
