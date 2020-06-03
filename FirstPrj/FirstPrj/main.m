b8004d60fda062bc28ccfcaba3bdfaaa28e1e36a
adorno <pch4623@gmail.com>
2020-06-03 23:10:17 +0900

    Start first iOS project
    최상위 클래스 NSObject 및 Getter/Setter

---
+++ FirstPrj/FirstPrj/main.m
@@ -0,0 +1,65 @@
+//
+//  main.m
+//  FirstPrj
+//
+//  Created by Philip on 2020/06/03.
+//  Copyright © 2020 Philip. All rights reserved.
+//
+
+// + : class method
+// - : instance method
+
+#import <Foundation/Foundation.h>
+
+@interface Vehicle : NSObject {
+    // member variable
+    int wheels;
+    int seats;
+}
+
+// member method
+-(void)setWheels:(int)w;
+-(void)setSeats:(int)s;
+-(int)wheels;
+-(int)seats;
+-(void)print;
+
+@end
+
+@implementation Vehicle
+-(void)setWheels:(int)w {
+    wheels = w;
+}
+-(void)setSeats:(int)s {
+    seats = s;
+}
+
+-(int)wheels {
+    return seats;
+}
+
+-(int)seats {
+    return wheels;
+}
+
+-(void)print {
+    NSLog(@"wheels : %i, seats : %i", wheels, seats);
+}
+
+@end
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // instance object 생성
+        Vehicle *vehicle = [[Vehicle alloc]init];   // [Vehicle new];
+        
+        //[Receiver Message]
+        [vehicle setWheels:4];
+        [vehicle setSeats:2];
+        //[vehicle print];
+        
+        NSLog(@"wheels : %i, seats : %i", [vehicle wheels], [vehicle seats]);
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ios-Projects
+iOS 개발 실습 프로젝트 모음 (Objective-C)
